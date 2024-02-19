1719dd9bb3217c9001356f050eb1287078eeb18f
Yume <hei.hajatiana@gmail.com>
2024-02-19 12:29:21 +0300

    feat(kata_serie): to_string_array

---
+++ kata_series/11_to_string_array.js
@@ -0,0 +1,31 @@
+/**
+ * TODO:
+ * I copied these items from GOOGLE, but now it is not a valid javascript
+ * can you put '' around items and ',' at the end of each item plssss
+ *
+ * it should look like ['One', 'Two', 'Etc']
+ */
+
+const arr = [
+  One
+  Two
+  Three
+  One
+  Two
+  Three
+  One
+  Two
+  Three
+  One
+  Two
+  Three
+  One
+  Two
+  Three
+  One
+  Two
+  Three
+  One
+  Two
+  Three
+] 
