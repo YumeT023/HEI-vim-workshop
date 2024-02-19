e638fe6f3361c8c3733749c6e97eb73df7334fba
Yume <hei.hajatiana@gmail.com>
2024-02-19 09:20:59 +0300

    refactor: sum.js -> lorem.js

--- 2 - navigating/lorem.js
+++ 2 - navigating/lorem.js
@@ -1,25 +1,25 @@
-// Improve your navigation skill
 /**
- * Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
- * Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
- * Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
- * Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
- * Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
- * Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
- * Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
- * Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
- * Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
- * Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
- * Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
- * Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
- * Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
- * Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
- * Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
- * Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
- * Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
- * Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
- * Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
- * Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
- * Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
- * Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
+ * TODO:
+ *
+ */
+
+
+/**
+ * Lorem ipsum dolor sit amet. Aut molestiae magnam aut reprehenderit magni eum rerum perspiciatis et voluptatibus
+ * culpa qui maxime tempore eos animi reiciendis aut commodi animi. Qui possimus voluptatem eos provident blanditiis
+
+ * sed molestiae impedit eos exercitationem autem et numquam quidem. Id eius impedit est ullam laboriosam ut voluptatem
+ * voluptatem et illo aspernatur eos odit nesciunt sed iusto placeat. Ab vitae quae aut voluptatem eveniet in repudiandae
+ * asperiores et facere asperiores. Vel quam sequi quo consequatur eaque et eaque dolor. At minima libero aut inventore adipisci
+ * ab fugit error qui voluptate culpa id expedita rerum quo maiores voluptatem et rerum explicabo! Ut internos optio et minus
+
+ * quam aut voluptatem reprehenderit sed possimus eligendi in harum adipisci est inventore illo eos molestias repellendus.
+ * Eum cumque eveniet est ducimus dolorem et autem ipsa aut inventore consequatur est autem laudantium et possimus nesciunt ut
+ * odio deserunt. A sequi odit aut omnis tempora aut quae deleniti ut corrupti deleniti est voluptas iure.
+
+ * Et quia sequi ea magni ipsa in officia asperiores. Et ullam consequatur ut tempore ullam ut excepturi optio et dolore
+ * incidunt sit cumque distinctio aut consequuntur nesciunt et praesentium distinctio. At velit minima qui fugiat autem et sunt
+ * omnis eos exercitationem rerum non molestiae quos eum enim sequi. Sit dolor tenetur aut laborum eveniet qui perferendis voluptas
+ * id nihil dolorem est officiis illum ut amet nisi ut dicta soluta. Et beatae recusandae et dignissimos velit qui quos officia
+ * sit voluptas neque.
  */
